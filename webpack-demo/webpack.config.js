8d4a9179dde4596d20d41ccddcd1c9a598c550b2
Sofiia <sofiiavoitkova@gmail.com>
2024-08-09 09:56:47 +0200

    chore: add ESLint configuration to Webpack setup

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -7,3 +7,30 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
 };
+
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new ESLintPlugin({
+      cache: true,
+      cacheLocation: "node_modules/.cache/eslint-webpack-plugin/.eslintcache",
+      configType: "eslintrc",
+      eslintPath: "eslint",
+      extensions: ["js"],
+      exclude: "node_modules",
+      resourceQueryExclude: [],
+      files: null,
+      fix: false,
+      formatter: "stylish",
+      lintDirtyModulesOnly: false,
+      threads: false,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: false,
+      quiet: false,
+      outputReport: false,
+    }),
+  ],
+};
